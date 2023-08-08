86f57667d1de9552a39eb0a96d7ed22a59bf3979
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-08 23:43:33 +0100

    test: tests for both the append and read function are failing

--- internal/log/store_test.go
+++ internal/log/store_test.go
@@ -24,7 +24,7 @@ func TestStoreAppendRead(t *testing.T) {
 	require.NoError(t, err)
 
 	testAppend(t, s)
-	// testRead(t, s)
+	testRead(t, s)
 	// testReadAt(t, s)
 
 	s, err = newStore((f))
@@ -35,9 +35,35 @@ func TestStoreAppendRead(t *testing.T) {
 
 func testAppend(t *testing.T, s *store) {
 	t.Helper()
+
+	print("did we get to this point \n")
+
 	for i := uint64(1); i < 4; i++ {
+		print("we are here, right?")
+
 		n, pos, err := s.Append(write)
+
+		print("something going wrong?", n, pos, err)
+
 		require.NoError(t, err)
 		require.Equal(t, pos+n, i*width)
+	}
+}
+
+func testRead(t *testing.T, s *store) {
+	t.Helper()
+	var pos uint64
+
+	for i := uint64(1); i < 4; i++ {
+
+		//print pos
+		//print width
+		print(pos)
+		print(width)
+
+		read, err := s.Read(pos)
+		require.NoError(t, err)
+		require.Equal(t, write, read)
+		pos += width
 	}
 }
