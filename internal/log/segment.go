cd05876eb260a56ac790df0e2384a3ac7e9730c3
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-18 21:42:07 +0100

    completed the segment section code. next: test that everything is setup correctly

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -75,3 +75,60 @@ func (s *segment) Append(record *api.Record) (offset uint64, err error) {
 	s.nextOffset++
 	return cur, nil
 }
+
+func (s *segment) Read(off uint64) (*api.Record, error) {
+	_, pos, err := s.index.Read(int64(off - s.baseOffset))
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := s.store.Read(pos)
+	if err != nil {
+		return nil, err
+	}
+
+	record := &api.Record{}
+	err = proto.Unmarshal(p, record)
+	return record, err
+}
+
+func (s *segment) IsMaxed() bool {
+	return s.store.size >= s.config.Segment.MaxStoreBytes || s.index.size >= s.config.Segment.MaxIndexBytes
+}
+
+func (s *segment) Remove() error {
+	if err := s.Close(); err != nil {
+		return err
+	}
+
+	if err := os.Remove(s.index.Name()); err != nil {
+		return err
+	}
+
+	if err := os.Remove(s.store.Name()); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *segment) Close() error {
+	if err := s.index.Close(); err != nil {
+		return err
+	}
+	if err := s.store.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// nearestMultipleOf returns the nearest multiple of k to j
+// we ust this to make sure that we're staying under the user's disk space limit
+func nearestMultipleOf(j, k uint64) uint64 {
+	// if j is positive, return the nearest multiple of k that is less than or equal to j
+	if j >= 0 {
+		return (j / k) * k
+	}
+	// if j is negative, return the nearest multiple of k that is greater than or equal to j
+	return ((j - k + 1) / k) * k
+}
