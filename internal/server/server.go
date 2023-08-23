7598f1993ce85bcabef9ecb2fe735ff213170ade
Edwards Moses <edwardsmoses3@gmail.com>
2023-08-24 00:03:23 +0100

    starting stages of implementing the server touse the gprc stubs we have

---
+++ internal/server/server.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/edwardsmoses/proglog/api/v1"
+	"google.golang.org/grpc"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
